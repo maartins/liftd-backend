51175cbb9ede1dc3993e9c9990a256af57fbee30
Martins <dzirko2@gmail.com>
2021-08-06 19:43:58 +0300

    Add routing and catcher

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use rocket::*;
+use rocket::fs::NamedFile;
+
+use std::path::{Path, PathBuf};
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join("index.html")).await.ok()
 }
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
+}
+
+#[catch(404)]
+async fn not_found() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join("index.html")).await.ok()
+}
+
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().register("/", catchers![not_found]).mount("/", routes![index, files])
 }
